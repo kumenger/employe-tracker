7dd5c23ed525a70ecfbdab4cc2c0ef2bff6378b5
kumenger <44558777+kumenger@users.noreply.github.com>
2023-03-21 15:05:32 -0600

    db routes

---
+++ helpers/questions.js
@@ -0,0 +1,27 @@
+const inquirer = require("inquirer");
+const questions = [{
+    type: 'list',
+    name: 'task',
+    message: 'What would you like to do?',
+    choices: [
+     new inquirer.Separator(),
+        'View all employees',
+        'View employees by manager',
+        'View all roles',
+        'View all departments',
+       new inquirer.Separator(),
+        'Add employee',
+        'Add role',
+        'Add department',
+        new inquirer.Separator(),
+        'Update employee',
+        new inquirer.Separator(),
+        'Delete employee',
+        'Delete role',
+        'Delete department',
+       
+    ],
+    pageSize: 28
+   
+}];
+module.exports=questions
